7be3eed0968bc76b60e334a07f92dac6cb10a4d8
HollyCakez <36055239+HollyCakez@users.noreply.github.com>
2018-03-19 17:38:35 -0700

    Finished maxGrade

--- functions.cpp
+++ functions.cpp
@@ -7,12 +7,40 @@
 using namespace std;
 double maxArr(double *ptrArr, int size)
 {
-  return 0;
+    double maxVal=0;
+    for(int i=0; i < size; i++)
+    {
+        if(ptrArr[i]>maxVal)
+        {
+            maxVal=ptrArr[i];
+        }
+        
+    }
+    
+    return maxVal;
 }
 
 double maxGrade(string filename)
 {
-  return 0;
+    
+    ifstream inputFile;
+    inputFile.open(filename);
+    
+    int sizeArr=0;
+    
+    inputFile >> sizeArr;
+    double *arr=new double[sizeArr];
+    double num;
+    int i=0;
+    while (inputFile >> num && i != sizeArr)
+    {
+        arr[i]=num;
+        i++;
+    }
+    double maxVal = maxArr(arr, sizeArr);
+    inputFile.close();
+    delete [] arr;
+    return maxVal;
 }
 
 void displayCircle(Circle circ)
